5b5886d0555f6231231501a4970516a3a330d0ed
hansonsks <hansonsks@gmail.com>
2024-06-21 18:44:06 +0100

    Added Tasks/Todo Controller and pages

---
+++ src/main/java/com/example/Todo_list/controller/TaskController.java
@@ -0,0 +1,140 @@
+package com.example.Todo_list.controller;
+
+import com.example.Todo_list.dto.TaskDTO;
+import com.example.Todo_list.dto.TaskTransformer;
+import com.example.Todo_list.entity.Priority;
+import com.example.Todo_list.entity.Task;
+import com.example.Todo_list.service.StateService;
+import com.example.Todo_list.service.TaskService;
+import com.example.Todo_list.service.ToDoService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final static Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskService taskService;
+    private final ToDoService toDoService;
+    private final StateService stateService;
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+                "principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id or " +
+                "@toDoServiceImpl.findToDoById(#todoId).collaborators" +
+                ".contains(@userServiceImpl.findUserById(principal.id))")
+    @GetMapping("/create/todos/{todo_id}")
+    public String showTaskCreationForm(@PathVariable("todo_id") Long id, Model model) {
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("todo", toDoService.findToDoById(id));
+        model.addAttribute("priorities", Priority.values());
+        logger.info("TaskController.showTaskCreationForm(): Displaying task creation form");
+        return "task-create";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+                "principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id or " +
+                "@toDoServiceImpl.findToDoById(#todoId).collaborators" +
+                ".contains(@userServiceImpl.findUserById(principal.id))")
+    @PostMapping("/create/todos/{todo_id}")
+    public String createTask(
+            @PathVariable("todo_id") Long id,
+            Model model,
+            @Validated @ModelAttribute("task") TaskDTO taskDTO,
+            BindingResult result
+    ) {
+        logger.info("TaskController.createTask(): Attempting to create task for todoId=" + id);
+
+        if (result.hasErrors()) {
+            logger.error("TaskController.createTask(): Error found in data received, aborting task creation");
+            model.addAttribute("todo", toDoService.findToDoById(id));
+            model.addAttribute("priorities", Priority.values());
+            return "task-create";
+        }
+
+        Task task = TaskTransformer.convertDTOToEntity(
+                taskDTO,
+                toDoService.findToDoById(taskDTO.getToDoId()),
+                stateService.findStateByName("New")
+        );
+
+        logger.info("TaskController.createTask(): Saving " + task);
+        taskService.save(task);
+        return String.format("redirect:/todos/%d/tasks", id);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+                "principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id or " +
+                "@toDoServiceImpl.findToDoById(#todoId).collaborators" +
+                ".contains(@userServiceImpl.findUserById(principal.id))")
+    @GetMapping("/{task_id}/update")
+    public String showTaskUpdateForm(@PathVariable("task_id") Long id, Model model) {
+        TaskDTO taskDTO = TaskTransformer.convertEntityToDTO(taskService.findTaskById(id));
+        model.addAttribute("task", taskDTO);
+        model.addAttribute("toDoId", taskDTO.getToDoId());
+        model.addAttribute("priorities", Priority.values());
+        model.addAttribute("states", stateService.findAllState());
+        logger.info("TaskController.showTaskUpdateForm(): Displaying task update form");
+        return "task-update";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+                "principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id or " +
+                "@toDoServiceImpl.findToDoById(#todoId).collaborators" +
+                ".contains(@userServiceImpl.findUserById(principal.id))")
+    @PostMapping("/{task_id}/update")
+    public String updateTask(
+            @PathVariable("task_id") Long id,
+            Model model,
+            @Validated @ModelAttribute TaskDTO taskDTO,
+            BindingResult result
+    ) {
+        logger.info("TaskController.updateTask(): Attempting to update task with taskId=" + id);
+
+        if (result.hasErrors()) {
+            logger.error("TaskController.updateTask(): Error found in data received, aborting task update");
+            model.addAttribute("priorities", Priority.values());
+            model.addAttribute("states", stateService.findAllState());
+            return "task-update";
+        }
+
+        taskDTO.setId(id);
+        Task task = TaskTransformer.convertDTOToEntity(
+                taskDTO,
+                toDoService.findToDoById(taskDTO.getToDoId()),
+                stateService.findStateByName(taskDTO.getState())
+        );
+
+        logger.info("TaskController.updateTask(): Updating " + task);
+        taskService.save(task);
+        return String.format("redirect:/todos/%d/tasks", task.getTodo().getId());
+    }
+
+    @GetMapping("/{task_id}/read")
+    public String displayTask(@PathVariable("task_id") Long id, Model model) {
+        Task task = taskService.findTaskById(id);
+        TaskDTO taskDTO = TaskTransformer.convertEntityToDTO(task);
+        model.addAttribute("task", taskDTO);
+        logger.info("TaskController.displayTask(): Displaying " + task);
+        return "task-info";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+                "principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id or " +
+                "@toDoServiceImpl.findToDoById(#todoId).collaborators" +
+                ".contains(@userServiceImpl.findUserById(principal.id))")
+    @GetMapping("/{task_id}/delete/todos/{todo_id}")
+    public String deleteTask(@PathVariable("task_id") Long taskId, @PathVariable("todo_id") Long todoId) {
+        logger.info("TaskController.deleteTask(): Deleting task with taskId" + taskId + " of todo with todoId=" + todoId);
+        taskService.deleteTaskById(taskId);
+        return String.format("redirect:/todos/%d/tasks", todoId);
+    }
+}
---
+++ src/main/java/com/example/Todo_list/controller/ToDoController.java
@@ -0,0 +1,164 @@
+package com.example.Todo_list.controller;
+
+import com.example.Todo_list.entity.Task;
+import com.example.Todo_list.entity.ToDo;
+import com.example.Todo_list.entity.User;
+import com.example.Todo_list.service.TaskService;
+import com.example.Todo_list.service.ToDoService;
+import com.example.Todo_list.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+@RequestMapping("/todos")
+@RequiredArgsConstructor
+public class ToDoController {
+
+    private final static Logger logger = LoggerFactory.getLogger(ToDoController.class);
+    private final ToDoService toDoService;
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @PreAuthorize("hasAuthority('ADMIN') or #ownerId==authentication.principal.id")
+    @GetMapping("/create/users/{owner_id}")
+    public String showToDoCreationForm(@PathVariable("owner_id") Long id, Model model) {
+        model.addAttribute("todo", new ToDo());
+        model.addAttribute("ownerId", id);
+        logger.info("ToDoController.showToDoCreationForm(): Displaying ToDo creation form");
+        return "todo-create";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or #ownerId==authentication.principal.id")
+    @PostMapping("/create/users/{owner_id}")
+    public String createToDo(
+            @PathVariable("owner_id") Long id,
+            @Validated @ModelAttribute("todo") ToDo toDo,
+            BindingResult result
+    ) {
+        logger.info("ToDoController.createToDo(): Attempting to create a ToDo item for user with userId=" + id);
+
+        if (result.hasErrors()) {
+            logger.info("ToDoController.createToDo(): Error found in data received, aborting ToDo creation");
+            return "todo-create";
+        }
+
+        toDo.setCreatedAt(LocalDateTime.now());
+        toDo.setOwner(userService.findUserById(id));
+
+        logger.info("ToDoController.createToDo(): Saving " + toDo);
+        toDoService.save(toDo);
+        return String.format("redirect:/todos/all/users/%d", id);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or " +
+            "principal.id==@toDoServiceImpl.findToDoById(#id).owner.id or " +
+            "@toDoServiceImpl.findToDoById(#id).collaborators" +
+            ".contains(@userServiceImpl.findUserById(principal.id))")
+    @GetMapping("/{id}/tasks")
+    public String displayToDo(@PathVariable("id") Long id, Model model) {
+        ToDo todo = toDoService.findToDoById(id);
+        List<Task> tasks = taskService.findAllTaskOfToDo(id);
+        List<User> users = userService.findAllUser()
+                                        .stream()
+                                        .filter(user -> !Objects.equals(user.getId(), todo.getOwner().getId()))
+                                        .toList();
+        List<Task> sortedTasks = tasks.stream().sorted(Comparator.comparing(Task::getPriority).reversed()).toList();
+
+        model.addAttribute("todo", todo);
+        model.addAttribute("tasks", sortedTasks);
+        model.addAttribute("users", users);
+
+        logger.info("ToDoController.displayToDo(): Displaying ToDo with toDoId=" + id);
+
+        return "todo-tasks";
+    }
+
+    @GetMapping("/{todo_id}/update/users/{owner_id}")
+    public String showToDoUpdateForm(
+            @PathVariable("todo_id") Long todoId,
+            @PathVariable("owner_id") Long ownerId,
+            Model model
+    ) {
+        ToDo toDo = toDoService.findToDoById(todoId);
+        model.addAttribute("todo", toDo);
+        logger.info("ToDoController.showToDoUpdateForm(): Displaying update form of " + toDo);
+        return "todo-update";
+    }
+
+    @PostMapping("/{todo_id}/update/users/{owner_id}")
+    public String updateToDo(
+            @PathVariable("todo_id") Long todoId,
+            @PathVariable("owner_id") Long ownerId,
+            @Validated @ModelAttribute("todo") ToDo todo,
+            BindingResult result)
+    {
+        logger.info("ToDoController.updateToDo(): Attempting to update ToDo with toDoId=" + todoId);
+
+        if (result.hasErrors()) {
+            todo.setOwner(userService.findUserById(ownerId));
+            logger.info("ToDoController.updateToDo(): Error found in data received, aborting ToDo update");
+            return "todo-update";
+        }
+
+        ToDo oldTodo = toDoService.findToDoById(todoId);
+        todo.setOwner(oldTodo.getOwner());
+        todo.setCollaborators(oldTodo.getCollaborators());
+
+        logger.info("ToDoController.updateToDo(): Updating " + todo);
+        toDoService.save(todo);
+        return String.format("redirect:/todos/all/users/%d", ownerId);
+    }
+
+    @GetMapping("/{todo_id}/delete/users/{owner_id}")
+    public String deleteToDo(@PathVariable("todo_id") Long todoId, @PathVariable("owner_id") Long ownerId) {
+        logger.info("ToDoController.deleteToDo(): Deleting ToDo with toDoId=" + todoId);
+        toDoService.deleteToDoById(todoId);
+        return String.format("redirect:/todos/all/users/%d", ownerId);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or #userId==authentication.principal.id")
+    @GetMapping("/all/users/{user_id}")
+    public String displayAllToDo(@PathVariable("user_id") Long userId, Model model) {
+        List<ToDo> todos = toDoService.findAllToDoOfUserId(userId);
+        List<ToDo> sortedTodos = todos.stream().sorted(Comparator.comparingLong(ToDo::getId)).toList();
+        model.addAttribute("todos", sortedTodos);
+        model.addAttribute("user", userService.findUserById(userId));
+        logger.info("ToDoController.displayAllToDo(): Displaying all ToDos of user with userId=" + userId);
+        return "todos-user";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id")
+    @GetMapping("/{todoId}/add")
+    public String addCollaborator(@PathVariable Long todoId, @RequestParam("user_id") Long userId) {
+        if (userId == -1) {
+            return String.format("redirect:/todos/%d/tasks", todoId);
+        }
+
+        logger.info("ToDoController.removeCollaborator(): " +
+                    "Adding " + toDoService.findToDoById(todoId) + " to" + userService.findUserById(userId));
+        toDoService.addCollaborator(todoId, userId);
+        return String.format("redirect:/todos/%d/tasks", todoId);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or authentication.principal.id==@toDoServiceImpl.findToDoById(#todoId).owner.id")
+    @GetMapping("/{todoId}/remove")
+    public String removeCollaborator(@PathVariable Long todoId, @RequestParam("user_id") Long userId) {
+        logger.info("ToDoController.removeCollaborator(): " +
+                    "Removing " + toDoService.findToDoById(todoId) + " from " + userService.findUserById(userId));
+        toDoService.removeCollaborator(todoId, userId);
+        return String.format("redirect:/todos/%d/tasks", todoId);
+    }
+
+}
